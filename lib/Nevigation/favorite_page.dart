21eacd46a30ed56870e4d3a98d32bfddb2eb3152
PratthanaManeejansuk051 <122779723+PratthanaManeejansuk051@users.noreply.github.com>
2024-09-21 14:44:24 +0700

    051

---
+++ lib/Nevigation/favorite_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FavoritePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'This is the Favorite Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
